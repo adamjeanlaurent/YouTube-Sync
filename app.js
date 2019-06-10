df30948a7fc1fe401031ca8152e730b8b3b1ad89
Adam Jean-Laurent <44792003+adamjeanlaurent@users.noreply.github.com>
2019-06-09 21:20:13 -0400

    documentation

--- app.js
+++ app.js
@@ -10,27 +10,37 @@ const io = socketio(server);
 app.use(express.static('public'));
 app.set("view engine", "ejs"); 
 
-
+// index page with embedded youtube player
 app.get('/', (req,res) => {
     res.render('index');
 });
 
+// connection event
 io.on('connection' , (socket) => {
     console.log('new connection');
+    
+    // play request
     socket.on('play' , () => {
         io.emit('globalPlay');
     });
+    
+    // pause request
     socket.on('pause', () => {
         io.emit('globalPause');
     });
+    
+    // skip request
     socket.on('skip' , (event) => {
         io.emit('globalSkip', event);
     });
+    
+    // disconnection event
     socket.on('disconnect', () => {
         console.log('A user has disconnected');
     });
 });
 
+// port where server resides
 server.listen(process.env.PORT || 3000, () => {
     console.log('Running On Port 3000!');
 });
